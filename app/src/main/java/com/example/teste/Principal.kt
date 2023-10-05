5dfcc1e670aa53e55b2a9fcda5d7efe5759b24d6
Luís Fernando <cavalcantevieiral@gmail.com>
2023-10-05 15:41:55 -0300

    Alterações na tela principal

--- app/src/main/java/com/example/teste/Principal.kt
+++ app/src/main/java/com/example/teste/Principal.kt
@@ -1,11 +1,31 @@
 package com.example.teste
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.teste.databinding.ActivityPrincipalBinding
+import com.example.teste.databinding.ActivityTelaDeCadastroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class Principal : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var binding: ActivityPrincipalBinding? = null
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        auth = Firebase.auth
+
+        binding?.btPropriedade?.setOnClickListener {
+            val navegarTelaPropriedade1 = Intent(this, CadastroDePropriedade1::class.java)
+            startActivity(navegarTelaPropriedade1)
+        }
     }
 }
